8d9af0bd31271e0c046593a9a539df446937e469
Janani S <Janani S@DESKTOP-13MT9AS>
2024-10-15 18:51:19 +0530

    Add project files.

---
+++ FormattingString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormattingString
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string director = "Sam Raimi", movie = "Spider man";
+            string msg=string.Format("{0} is the director of {1}", director, movie);
+            string msg2 = $"{director} is the director of {movie}";
+            Console.WriteLine(msg);
+            Console.WriteLine(msg2);
+            Console.ReadKey();
+        }
+    }
+}
